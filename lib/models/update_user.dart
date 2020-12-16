2fb39dde425efccae5d6b661b65e4ffb90959cae
Bryan Bill <46356585+CocoGinger@users.noreply.github.com>
2020-12-16 09:57:09 +0300

    created update user details  model class

---
+++ lib/models/update_user.dart
@@ -0,0 +1,13 @@
+Class UpdateUserModel{
+  final File mediaFile;
+  final String displayname;
+  final String username;
+  final String statusUpdate;
+  
+  UpdateUserModel({
+    this.mediaFile,
+    this.displayName;
+    this.username,
+    this.statusUpdate;
+  });
+}
